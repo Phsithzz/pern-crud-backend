17f0a1a674d40e75eba4e85639bd8b25deac7055
Phsithzz <phoosith2548@gmail.com>
2025-07-21 17:04:18 +0700

    Add Comment Code

--- db.js
+++ db.js
@@ -18,3 +18,25 @@ db.on("error", (err) => {
 });
 
 export const query = (text, params) => db.query(text, params);
+
+// import pg from "pg";
+// import dotenv from "dotenv";
+
+// dotenv.config();
+
+// const db = new pg.Client({
+//   user: process.env.PG_USER,
+//   host: process.env.PG_HOST,
+//   database: process.env.PG_DATABASE,
+//   password: process.env.PG_PASSWORD,
+//   port: process.env.PG_PORT,
+// });
+
+// db.connect();
+
+// db.on("error", (err) => {
+//   console.log("Unexpected error on idle client", err);
+//   process.exit(-1);
+// });
+
+// export const query = (text, params) => db.query(text, params);
